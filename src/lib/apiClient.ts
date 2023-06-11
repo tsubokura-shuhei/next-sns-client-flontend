84e60f017450e67538d456f97cf76c4719be27cb
tsubokura-shuhei <repeat2639@gmail.com>
2023-06-11 12:13:25 +0900

    baseurl

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8080/api",
+  // baseURL: "http://localhost:8080/api",
+  baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
   headers: {
     "Content-Type": "application/json",
   },
